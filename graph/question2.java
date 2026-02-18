fa5451b3524696660f27186516ef89f77fba9f6b
Nikhil Sharma <sharma.nikhil23062000@gmail.com>
2026-02-18 08:58:32 -0500

    Another question

---
+++ graph/question2.java
@@ -0,0 +1,85 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class question2 {
+
+    public int findCircleNum(int[][] isConnected) {
+        ArrayList<ArrayList<Integer>> data = new ArrayList<>();
+
+        for (int i = 0; i < isConnected.length; i++) {
+            data.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < isConnected.length; i++) {
+            for (int j = 0; j < isConnected[i].length; j++) {
+                if (isConnected[i][j] == 1) {
+                    data.get(i).add(j);
+                }
+            }
+        }
+
+        return dfs(data);
+    }
+
+    public int dfs(ArrayList<ArrayList<Integer>> adj) {
+        boolean[] visited = new boolean[adj.size()];
+        int result = 0;
+
+        for (int j = 0; j < adj.size(); j++) {
+            if (visited[j])
+                continue;
+
+            visited[j] = true;
+            dfs(adj, visited, j);
+            result++;
+        }
+
+        return result;
+    }
+
+    public void dfs(ArrayList<ArrayList<Integer>> adj, boolean[] visited, int node) {
+
+        ArrayList<Integer> neighbours = adj.get(node);
+
+        for (int i = 0; i < neighbours.size(); i++) {
+            if (visited[neighbours.get(i)] == false) {
+                visited[neighbours.get(i)] = true;
+                dfs(adj, visited, neighbours.get(i));
+            }
+        }
+
+        return;
+    }
+
+    public int bfs(ArrayList<ArrayList<Integer>> adj) {
+        int result = 0;
+        boolean[] visited = new boolean[adj.size()];
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int j = 0; j < adj.size(); j++) {
+            if (visited[j])
+                continue;
+            result++;
+            queue.add(j);
+            visited[j] = true;
+
+            while (queue.isEmpty() == false) {
+                Integer temp = queue.poll();
+
+                ArrayList<Integer> neighbours = adj.get(temp);
+
+                for (int i = 0; i < neighbours.size(); i++) {
+                    if (visited[neighbours.get(i)] == false) {
+                        visited[neighbours.get(i)] = true;
+                        queue.add(neighbours.get(i));
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
--- graph/readme.md
+++ graph/readme.md
@@ -7,6 +7,7 @@ Always make sure we traverse over all of the components as nodes can be non conn
 ## Practice Problems
 
 1. [BFS and DFS](#bfs-and-dfs)
+2. [Number of Provinces](#Number-of-provinces)
 
 ## BFS and DFS
 
@@ -20,3 +21,14 @@ For dfs no need to use queue just keep going deep on edges
 | -------- | --------------- | ---------------- | -------------------------- |
 | **BFS**  | _O(V+E)_        | _O(V)_           | Queue occupy extra storage |
 | **DFS**  | _O(V+E)_        | _O(V)_           | Recursive stack            |
+
+## Number of Provinces
+
+```
+Again can be done with both bfs and dfs as curecntly i converted the array to adjacency list it cause V^2 of time complexicity but can be in V+E too
+```
+
+| Approach | Time Complexity | Space Complexity | Why                        |
+| -------- | --------------- | ---------------- | -------------------------- |
+| **BFS**  | _O(v^2)+O(V+E)_ | _O(V)_           | Queue occupy extra storage |
+| **DFS**  | _O(v^2)+O(V+E)_ | _O(V)_           | Recursive stack            |
