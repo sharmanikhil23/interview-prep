6b2ac2e1f7e541935bb19e8135158649557e22c1
Nikhil Sharma <sharma.nikhil23062000@gmail.com>
2026-03-03 07:21:31 -0500

    Another question word ledder 2

---
+++ graph/question17.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class question17 {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        return bfs(numCourses, prerequisites);
+    }
+
+    public int[] bfs(int V, int[][] edges) {
+        ArrayList<ArrayList<Integer>> data = fixingData(V, edges);
+        int[] inDegree = new int[V];
+        Queue<Integer> temp = new LinkedList<>();
+
+        for (int[] e : edges) {
+            inDegree[e[0]]++;
+        }
+
+        for (int i = 0; i < V; i++) {
+            if (inDegree[i] == 0) {
+                temp.add(i);
+            }
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        while (temp.isEmpty() == false) {
+            int tempData = temp.poll();
+            result.add(tempData);
+
+            ArrayList<Integer> neighbours = data.get(tempData);
+
+            for (int i = 0; i < neighbours.size(); i++) {
+                inDegree[neighbours.get(i)]--;
+
+                if (inDegree[neighbours.get(i)] == 0) {
+                    temp.add(neighbours.get(i));
+                }
+            }
+        }
+
+        return result.size() == V ? result.stream()
+                .mapToInt(i -> i)
+                .toArray() : new int[] {};
+
+    }
+
+    private ArrayList<ArrayList<Integer>> fixingData(int v, int[][] edges) {
+        ArrayList<ArrayList<Integer>> data = new ArrayList<>();
+        for (int i = 0; i < v; i++) {
+            data.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < edges.length; i++) {
+            data.get(edges[i][1]).add(edges[i][0]);
+        }
+
+        return data;
+    }
+}
